5ab17c5f6cae510932f0a6651767f38a1b542615
Clay Carpenter <claycarpenter@gmail.com>
2015-04-20 02:16:52 +0000

    Imports initial site build script.

---
+++ build.js
@@ -0,0 +1,17 @@
+var Metalsmith = require('metalsmith'),
+    markdown = require('metalsmith-markdown'),
+    jadeTemplater = require('metalsmith-jade-templater');
+
+Metalsmith(__dirname)
+    .source('./src/')
+    .destination('./output/')
+    .use(markdown())
+    .use(jadeTemplater())
+    .build(function(err, files) {
+        console.log('Build function...');
+        
+        if (err) throw err;
+        
+        console.log('Build successful.');
+        console.log('Output files:', Object.keys(files));
+    });
