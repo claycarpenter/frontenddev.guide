38e640e924ea2ddadf5f6b7451b5a8a65a5b1694
Clay Carpenter <claycarpenter@gmail.com>
2015-04-22 00:34:49 +0000

    Includes Metalsmith drafts plugin into build process.

--- build.js
+++ build.js
@@ -1,10 +1,12 @@
 var Metalsmith = require('metalsmith'),
     markdown = require('metalsmith-markdown'),
-    jadeTemplater = require('metalsmith-jade-templater');
+    jadeTemplater = require('metalsmith-jade-templater'),
+    drafts = require('metalsmith-drafts');
 
 Metalsmith(__dirname)
     .source('./src/')
     .destination('./output/')
+    .use(drafts())
     .use(markdown())
     .use(jadeTemplater(__dirname + '/templates'))
     .build(function(err, files) {
