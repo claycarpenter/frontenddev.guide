bc07bd56fdd6ee222ba9a2570affbfb8fe808b89
Clay Carpenter <claycarpenter@gmail.com>
2015-04-23 00:37:44 +0000

    Updates build architecture to support conditional drafts mode.

--- build.js
+++ build.js
@@ -3,17 +3,28 @@ var Metalsmith = require('metalsmith'),
     jadeTemplater = require('metalsmith-jade-templater'),
     drafts = require('metalsmith-drafts');
 
-Metalsmith(__dirname)
-    .source('./src/')
-    .destination('./output/')
-    .use(drafts())
+var isDraftMode = Boolean(process.argv[2]);
+
+var metalsmith = Metalsmith(__dirname)
+                    .source('./src/')
+                    .destination('./output/');
+
+if (!isDraftMode) {
+    // Draft mode is disabled, use drafts plugin to excise draft articles.
+    metalsmith.use(drafts())
+}
+
+// Continue constructing the build pipeline.
+metalsmith
     .use(markdown())
-    .use(jadeTemplater({baseTemplatesDir: __dirname + '/templates'}))
-    .build(function(err, files) {
-        console.log('Build function...');
-        
-        if (err) throw err;
-        
-        console.log('Build successful.');
-        console.log('Output files:', Object.keys(files));
-    });
+    .use(jadeTemplater({baseTemplatesDir: __dirname + '/templates'}));
+
+// Execute the build process.
+metalsmith.build(function(err, files) {
+    console.log('Building in', (isDraftMode ? 'draft' : 'production'), 'mode.');
+    
+    if (err) throw err;
+    
+    console.log('Build successful.');
+    console.log('Output files:', Object.keys(files));
+});
